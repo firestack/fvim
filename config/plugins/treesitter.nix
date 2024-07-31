be1378f81107adc065ce932f4ae92fce17a75b66
Kayla Firestack <kfirestack@mbta.com>
2024-07-30 20:17:20 -0400

    feat: highlight and search settings

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -2,6 +2,8 @@
 	plugins.treesitter= {
 		enable = true;
 		settings.indent.enable = true;
+		settings.highlight.enable = true;
+		settings.incremental_selection.enable = true;
 	 	folding = true;
 	};
 }
